5a35d5fb3acd14fa1e3b9ece18ad453adbb9db92
tejasvisinghjodha <veenitejas@gmail.com>
2025-08-05 16:20:20 +0530

    Resolve merge conflict in getMovies.js

--- netlify/functions/getMovies.js
+++ netlify/functions/getMovies.js
@@ -1,22 +1,22 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function () {
-  const API_KEY = process.env.TMDB_API_KEY;
+    const API_KEY = process.env.TMDB_API_KEY;
 
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data.results),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch movies." }),
-    };
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.results),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to fetch movies." }),
+        };
+    }
 };
